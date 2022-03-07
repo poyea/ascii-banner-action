62449c574ae8f4d0f950850d5deaf2d7fa853d2f
John Law <johnlaw.po@gmail.com>
2022-03-08 01:18:31 +0800

    feat: Add basic style (#2)
    * feat: add basic ascii style
    
    * fix: handle cases
    
    * feat: handle font style
    
    * ci: add test workflow
    
    * fix: update ci

--- src/convert.ts
+++ src/convert.ts
@@ -1,11 +1,29 @@
-const convert = async (word: string, style: string | undefined): Promise<string> => {
-  return new Promise(resolve => {
-    if (style) {
-      resolve(`${word} with ${style}`);
-    } else {
-      resolve(word);
-    }
-  });
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import art from "ascii-art";
+import * as core from "@actions/core";
+
+const convert = async (word: string, style: string | undefined) => {
+  style = style?.toLowerCase();
+  const artFont = (style: string) => {
+    // Sadly it only works properly this way
+    art.font(word, style, (err: undefined, rendered: string) => {
+      if (!err) {
+        core.setOutput("banner", rendered);
+        core.info(rendered);
+        core.debug(`Done printing ${word} in ASCII.`);
+      }
+    });
+  };
+  switch (style) {
+    case "rusted":
+      artFont("rusted");
+      break;
+    case "doom":
+    default:
+      artFont("Doom");
+      break;
+  }
 };
 
 export default convert;
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,10 @@ import convert from "./convert";
 async function run (): Promise<void> {
   try {
     const word: string = core.getInput("word");
-    const style: string = core.getInput("style");
+    const style: string = core.getInput("style").toLowerCase();
     core.debug(`Printing ${word} in ASCII...`);
 
-    const banner = await convert(word, style);
-    core.setOutput("banner", banner);
-    core.info(banner);
-    core.debug(`Done printing ${word} in ASCII.`);
+    convert(word, style);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
