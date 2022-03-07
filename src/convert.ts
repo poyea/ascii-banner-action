7dd0125600096809ef2421e7b72c94ecb492d98e
John Law <johnlaw.po@gmail.com>
2022-03-07 16:05:07 +0000

    feat: add basic ascii style

--- src/convert.ts
+++ src/convert.ts
@@ -1,11 +1,24 @@
-const convert = async (word: string, style: string | undefined): Promise<string> => {
-  return new Promise(resolve => {
-    if (style) {
-      resolve(`${word} with ${style}`);
-    } else {
-      resolve(word);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import art from "ascii-art";
+
+const convert = async (word: string, style: string | undefined) => {
+  try {
+    switch (style) {
+      case "doom":
+      {
+        const rendered = await art.font(word, style).completed();
+        return rendered;
+      }
+      default:
+      {
+        const rendered = await art.font(word, "doom").completed();
+        return rendered;
+      }
     }
-  });
+  } catch (err) {
+    return word;
+  }
 };
 
 export default convert;
